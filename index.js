abd0e75bc3402c9baab54d5ff930f1b1408b52b6
bayjidmia <mdbayjidmia982@gmail.com>
2025-12-09 07:49:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 3000;
+require("dotenv").config();
+
+// midleware
+app.use(express.json());
+app.unsubscribe(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
